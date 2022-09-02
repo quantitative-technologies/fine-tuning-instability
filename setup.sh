d0785ca3962e2cba9421e91ac1ae2457c02f5b6f
James Hirschorn <James.Hirschorn@quantitative-technologies.com>
2022-09-03 03:39:35 +0700

    Depends on aenum

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 pip install cloud-tpu-client==0.10 torch==1.12.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.12-cp37-cp37m-linux_x86_64.whl
 pip install datasets==1.11.0
+pip install aenum
 
 git clone https://github.com/huggingface/transformers.git
 cd transformers; git checkout tags/v4.20.1; pip install -e .; cd ..
