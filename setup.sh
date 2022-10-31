d9471094ded67cb35fa649b02be9a26abeb2fb88
James Hirschorn <James.Hirschorn@quantitative-technologies.com>
2022-10-31 19:47:51 -0400

    Revert "Problem with setup.sh: does not detect TPU properly"
    This reverts commit 04ed4ac607c5f94f833167f95f1ef9886050f704.

--- setup.sh
+++ setup.sh
@@ -2,17 +2,19 @@
 
 # Our experiments were done with torch-1.12.0, datasets-1.11.0, transformers tag v4.20.1 and the torch_xla-1.12 wheel below.
 
-# Check for TPU
-if [ "$1" == 'TPU' ]
-then
-    printf "\nUsing TPU. Installing torch_xla...\n"
-    pip install cloud-tpu-client==0.10 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.12-cp37-cp37m-linux_x86_64.whl
-else
-    printf "\nNot using TPU\n"
-fi
-
 pip install torch==1.12.0
 pip install datasets==1.11.0
+
+# Check for TPU
+output="$(env | grep TPU)"
+
+if [[ -n $output ]]
+then
+    printf "\nTPU detected. Installing torch_xla...\n"
+    pip install cloud-tpu-client==0.10 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.12-cp37-cp37m-linux_x86_64.whl
+else
+    printf "\nNo TPU detected\n"
+fi
 
 # Additional dependency for our library
 pip install aenum
